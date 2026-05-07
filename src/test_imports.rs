a60fb83456cc041fea16dcca5b7cfaeb3e196c42
YuzukoUnderson <rikuichi0212@gmail.com>
2026-05-08 01:00:29 +0900

    [fix] .gitignoreを修正し、集約したソースコードと設定ファイルをGit追跡対象に追加

---
+++ src/test_imports.rs
@@ -0,0 +1,7 @@
+use rust_mcp_sdk::mcp_handlers::mcp_server_handler::ServerHandler;
+use rust_mcp_sdk::mcp_traits::mcp_server::McpServer;
+use rust_mcp_sdk::mcp_server::server_runtime;
+
+fn main() {
+    println!("Imports successful?");
+}
